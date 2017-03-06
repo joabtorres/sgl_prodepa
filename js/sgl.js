0e844606fb600d979008e020e24deebfd15afc53
Joab Torres <joabtorres1508@gmail.com>
2017-03-06 12:49:14 -0300

    criado menu de navegação do sistema

--- js/sgl.js
+++ js/sgl.js
@@ -4,6 +4,16 @@ function fundo_tela_login() {
         $('body').css('background-color', '#EBEFF2');
     }
 }
+/*
+* @author Joab Torres Alencar
+* @description Está função submite o forumlário de buscar rápida que está no menu principal
+* Menu principal 
+*/
+function submit_form_navbar(){
+    if(document.nSearchSGL){
+        document.nSearchSGL.submit();
+    }
+}
 
 $(document).ready(function(){
     fundo_tela_login();
